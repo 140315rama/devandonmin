d250cc12780b34b287ae3eb46c5d35ba1ae13d2d
Fajrin Rama Sanjaya <fajrin.sanjaya@usg.co.id>
2023-11-02 16:09:07 +0700

    new file:   .gitignore new file:   .vscode/extensions.json new file:   include/DevAndonMin.h new file:   include/README new file:   platformio.ini new file:   src/main.cpp

---
+++ include/DevAndonMin.h
@@ -0,0 +1,76 @@
+#ifndef INCLUDE_DEFANDONMIN_H_
+#define INCLUDE_DEFANDONMIN_H_
+
+#include "WiFi.h"
+#include "HTTPClient.h"
+#include "ArduinoJson.h"
+
+#define GPIO_LED_GREEN      21
+#define GPIO_LED_YELLOW     23
+#define GPIO_LED_RED        22
+#define GPIO_BUZZER         19
+
+#define WIFI_SSID           "USGRENT71"
+#define WIFI_PASSWD         "1234567890"
+#define WIFI_TIMEOUT_MS     20000
+
+#define LED_GREEN_MODE      20
+#define LED_YELLOW_MODE     21
+#define LED_RED_MODE        22
+
+#define API_URL             "http://10.61.0.21:7993/getSummaryCounterLine?id_dev=C3QC1001"
+#define API_USER            "ACTIFY"
+#define API_PASSWD          "12345678"
+
+class Connection {
+    public:
+        void connect();
+};
+Connection connection;
+
+class Task {
+    public:
+        String payload;
+        int httpCode;
+        int defect;
+        int pending;
+        int repaired;
+        void getRestAPI();
+        void readData(void *params);
+        void setAPIStat(int8_t Status) {
+            status = Status;
+        }
+        int8_t restAPIStat() const {
+		    return status;
+        }
+
+    private:
+      	int8_t status;
+
+};
+Task task;
+
+class Buzzer {
+    public:
+        void configure();
+};
+Buzzer buzzer;
+
+class LEDIndicator {
+    public:
+        void configure();
+        void light(int8_t mode);
+        void setLightMode(int8_t Mode) {
+            mode = Mode;
+        }
+        int8_t getLEDMode() const {
+		    return mode;
+        }
+
+    private:
+      	int8_t mode;
+};
+
+LEDIndicator led;
+
+#endif /*INCLUDE_DEFANDONMIN_H_*/
---
+++ src/main.cpp
@@ -0,0 +1,142 @@
+#include <Arduino.h>
+#include "DevAndonMin.h"
+
+void setup() {
+  Serial.begin(9600);
+  delay(2000);
+  pinMode(LED_BUILTIN, OUTPUT);
+  buzzer.configure();
+  led.configure();
+  connection.connect();
+
+}
+
+void loop() {
+  if (WiFi.status() == WL_CONNECTED) {
+    task.readData(&task);
+    int mode = led.getLEDMode();
+    led.light(mode);
+    digitalWrite(BUILTIN_LED, HIGH);
+    delay(150);
+    digitalWrite(BUILTIN_LED, LOW);
+    delay(200);
+  } else {
+    digitalWrite(BUILTIN_LED, LOW);
+  }
+}
+
+void Connection::connect() {
+  delay(500);
+  Serial.print("Connecting to Network... ");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWD);
+
+  unsigned long startAttempTime = millis();
+
+  while (WiFi.status() != WL_CONNECTED && millis() - startAttempTime < WIFI_TIMEOUT_MS) {
+    Serial.print(".");
+    delay(100);
+  }
+
+  if(WiFi.status() != WL_CONNECTED) { 
+    Serial.println();
+    Serial.println("Failed");
+  }
+  else { 
+    Serial.println();
+    Serial.print("Connected With IP :  ");
+    Serial.println(WiFi.localIP());
+  }
+}
+
+void Task::getRestAPI() {
+  delay(500);
+  Serial.print("Getting Data From API... ");
+  WiFiClient client;
+  HTTPClient http;
+  http.begin(client, API_URL); 
+  http.setAuthorization(API_USER,API_PASSWD);
+  httpCode = http.GET();
+  payload = http.getString();
+  if (httpCode = 200) {
+     setAPIStat(true);
+     Serial.print("API OKE \n\r");
+  } else {
+    setAPIStat(false);
+    Serial.print("API FAIL \n\r");
+  }
+}
+
+void Task::readData(void *params) { 
+  Task *config_obj = (Task*) params;
+  task.getRestAPI();
+  if (config_obj->restAPIStat()) {
+    DynamicJsonDocument doc (1024);
+    deserializeJson(doc, payload);
+    JsonObject summary = doc["summary"];
+    defect = summary["defect_val"];
+    repaired = summary["repaired_val"];
+    pending = defect-repaired;
+    Serial.print("Defect : ");
+    Serial.println(defect);
+    Serial.print("Repaired : ");
+    Serial.println(repaired);
+    Serial.print("Pending : ");
+    Serial.println(pending);
+    if (pending<=2) {
+      led.setLightMode(20);
+    } else if (pending>2 && pending<=4) {
+      led.setLightMode(21);
+    } else {
+      led.setLightMode(22);
+    }
+  } else {
+    Serial.println("Rest API Error");
+  }
+}
+
+void Buzzer::configure() {
+  Serial.print("Configuring Buzzer.... ");
+  pinMode(GPIO_BUZZER,OUTPUT);
+  Serial.print("Done\n\r");
+}
+
+void LEDIndicator::configure() {
+  Serial.print("Configuring LED Indicator.... ");
+  pinMode(GPIO_LED_GREEN,OUTPUT);
+  pinMode(GPIO_LED_YELLOW,OUTPUT);
+  pinMode(GPIO_LED_RED,OUTPUT);
+  Serial.print("Done\n\r");
+}
+
+void LEDIndicator::light(int8_t mode) {
+  switch (mode)
+  {
+  case LED_GREEN_MODE :
+    digitalWrite(GPIO_LED_GREEN,LOW);
+    digitalWrite(GPIO_LED_YELLOW,HIGH);
+    digitalWrite(GPIO_LED_RED,HIGH);
+    digitalWrite(GPIO_BUZZER,HIGH);
+    Serial.println("GREEN");
+    break;
+
+  case LED_YELLOW_MODE : 
+    digitalWrite(GPIO_LED_GREEN,HIGH);
+    digitalWrite(GPIO_LED_YELLOW,LOW);
+    digitalWrite(GPIO_LED_RED,HIGH);
+    digitalWrite(GPIO_BUZZER,HIGH);
+    break;
+
+  case LED_RED_MODE : 
+    digitalWrite(GPIO_LED_GREEN,HIGH);
+    digitalWrite(GPIO_LED_YELLOW,HIGH);
+    digitalWrite(GPIO_LED_RED,LOW);
+    Serial.println("RED");
+    digitalWrite(GPIO_BUZZER,LOW);
+    break;
+  
+  default :
+      LED_GREEN_MODE;
+    break;
+  }
+}
